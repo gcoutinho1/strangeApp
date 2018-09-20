5c141610d06ba5279c90c2f965329b6b6533938f
Guilherme Coutinho <guilhermecoutinho7777@gmail.com>
2018-09-20 20:46:15 -0300

    testes

--- app/src/main/java/coutinhodeveloper/com/strangeapp/MainActivity.java
+++ app/src/main/java/coutinhodeveloper/com/strangeapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 
 import com.parse.FindCallback;
 import com.parse.GetCallback;
+import com.parse.LogInCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
@@ -13,8 +14,13 @@ import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.ParseInstallation;
 import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
 
 import java.util.List;
+
+/** Created by Guilherme Coutinho
+ *  on 19/09/2018
+ */
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,62 +29,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*ParseObject pontuacao = new ParseObject("Pontuacao");
-        pontuacao.put("nome", "Gerald");
-        pontuacao.put("pontos", 150);
-        pontuacao.saveInBackground(new SaveCallback() {
+       /* // cadastro de usuarios
+        ParseUser usuario = new ParseUser();
+        usuario.setUsername("guilherme");
+        usuario.setPassword("123456");
+        usuario.setEmail("guilherme@gmail.com");
+
+        // logar
+        usuario.signUpInBackground(new SignUpCallback() {
             @Override
             public void done(ParseException e) {
-                if (e == null){ //significa que não tem erro
-                    Log.i("salvarPontos", "dados salvos");
+                if (e==null){
+                    Log.i ("cadastrarUsuario", "sucesso ao cadastrar ");
                 }else {
-                    Log.i("salvarPontos", "erro ao salvar");
+                    Log.i ("cadastrarUsuario", "erro ao cadastrar - " + e.getMessage());
                 }
             }
         }); */
 
-        /*ParseQuery<ParseObject> consulta = ParseQuery.getQuery("Pontuacao");
-        consulta.getInBackground("PTZBjb6bYi", new GetCallback<ParseObject>() {
+        /** verificar usuario
+         *
+         */
+        /*ParseUser.logOut();
+        if (ParseUser.getCurrentUser() != null){
+            Log.i ("loginUsuario", "logado ");
+
+        }else {
+            Log.i ("loginUsuario", "deslogado ");
+
+        } */
+
+        /* ParseUser.logInInBackground("guilherme", "123456", new LogInCallback() {
             @Override
-            public void done(ParseObject object, ParseException e) {
-
-                if (e==null){
-                    object.put("pontos", 333);
-                    object.saveInBackground();
-                }else {
-                    Log.i("consulta", "erro na consulta");
-                }
-            }
-        });  */
-
-
-        ParseQuery<ParseObject> filtro = ParseQuery.getQuery("Pontuacao");
-        // aplicando filtros na listagem dos objetos
-        //filtro.whereGreaterThan("pontos", 800);
-        //filtro.whereGreaterThanOrEqualTo("pontos", 777);
-        //filtro.whereLessThanOrEqualTo("pontos",777);
-        //filtro.whereEndsWith("nome","gu");
-        //filtro.whereStartsWith("nome","G");
-        //filtro.addAscendingOrder("pontos");
-        //filtro.setLimit(1);
-
-        //listar dados
-        filtro.findInBackground(new FindCallback<ParseObject>() {
-            @Override
-            public void done(List<ParseObject> objects, ParseException e) {
-
-                if (e == null){ //efetuado listagem
-                    for (ParseObject object : objects){
-                        Log.i("listagem", "objetos - Nome: " + object.get("nome") + " ponto: " + object.get("pontos"));
-
-
-                    }
+            public void done(ParseUser user, ParseException e) {
+                if (e==null){// logado
+                    Log.i("verificacaoLogin", "login com sucesso");
 
                 }else {
-                    Log.i("listagem", "erro na listagem -" + e.getMessage());
+                    Log.i("verificacaoLogin", "login falhou" + e.getMessage());
+
                 }
             }
-        });
+        }); */
 
 
     }
