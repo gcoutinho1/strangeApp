6c219ed3d5232b20bb4208f3d06dde1fd9849208
Guilherme Coutinho <guilhermecoutinho7777@gmail.com>
2018-09-21 17:33:56 -0300

    imagens salvando no banco de dados corretamente

--- app/src/main/java/coutinhodeveloper/com/strangeapp/activity/MainActivity.java
+++ app/src/main/java/coutinhodeveloper/com/strangeapp/activity/MainActivity.java
@@ -1,6 +1,9 @@
 package coutinhodeveloper.com.strangeapp.activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -11,12 +14,14 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.GetCallback;
 import com.parse.LogInCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
@@ -24,6 +29,10 @@ import com.parse.ParseInstallation;
 import com.parse.SaveCallback;
 import com.parse.SignUpCallback;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import coutinhodeveloper.com.strangeapp.R;
@@ -88,10 +97,72 @@ public class MainActivity extends AppCompatActivity {
                 // fazer algo
                 return true;
             case R.id.action_share:
+                compartilharFoto();
                 // fazer algo
                 return true;
                 default:
                     return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void compartilharFoto(){
+
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent,1);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // testando processo de retorno dos dados
+        if (requestCode==1 && resultCode==RESULT_OK && data != null){
+
+            //recuperar local do recurso
+            Uri localImagemSelecionado = data.getData();
+
+            //recupera a imagem do local que foi selecionada
+            try {
+                Bitmap imagem = MediaStore.Images.Media.getBitmap(getContentResolver(),localImagemSelecionado);
+
+                //comprimir formato png
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                imagem.compress(Bitmap.CompressFormat.PNG,75,stream);
+                // criar array de bytes
+                byte[] byteArray = stream.toByteArray();
+
+                //mudando nome do arquivo no parse
+                SimpleDateFormat dateFormat = new SimpleDateFormat("ddmmaaaahhmmss");
+                String nomeImagem = dateFormat.format(new Date());
+
+                //  criar arquivo do formato proprio do parse
+                ParseFile arquivoParse = new ParseFile(nomeImagem +"imagem.png",byteArray);
+
+                // monta objeto pra salvar no parse
+                ParseObject parseObject = new ParseObject("Imagem");
+                parseObject.put("username",ParseUser.getCurrentUser().getUsername());
+                parseObject.put("imagem",arquivoParse);
+
+                // salvando os dados
+                parseObject.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+
+                        if (e==null){//sucesso
+                            Toast.makeText(getApplicationContext(),"Sua imagem foi postada!",Toast.LENGTH_LONG).show();
+
+
+                        }else {//erro
+                            Toast.makeText(getApplicationContext(),"Erro ao postar sua imagem",Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 
